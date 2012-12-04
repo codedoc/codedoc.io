daaa66d446545e617f2ef5342046582eea7a2398
Federico Spini <federico.spini@gmail.com>
2012-12-03 16:23:40 -0800

    new folder structure

---
+++ client/components/spini-backbone-history/component.json
@@ -0,0 +1,16 @@
+{
+  "name": "backbone-history",
+  "repo": "spini/backbone-history",
+  "description": "Backbone History component",
+  "version": "0.0.1",
+  "keywords": [],
+  "dependencies": {
+    "component/underscore": "*",
+    "component/jquery": "*",
+    "timoxley/backbone-events": "*"
+  },
+  "development": {},
+  "scripts": [
+    "index.js"
+  ]
+}
--- server/server.js
+++ server/server.js
@@ -11,41 +11,61 @@ var credentials = {
 };
 
 var express = require('express');
-var request = require('superagent');
-var join = require('path').join;
-var redox = require('redox');
-var finder = require('github-finder')(credentials);
+// var request = require('superagent');
+// var join = require('path').join;
+// var redox = require('redox');
+// var finder = require('github-finder')(credentials);
+// var build = require('./build.js');
 
 var server = express();
 
 server
-  .use(express.bodyParser())
-  .use(express.static(__dirname + '/../public'))
-  .listen(8002);
+  // .use(express.favicon())
+  // .use(express.logger('dev'))
+  .use(express.static(__dirname + '/public'))
+  // .use(express.bodyParser());
 
-server.get('/:user/:project', function(req, res) {
-  var params = req.params;
-  res.redirect('/#/docs/' + params.user + '/' + params.project);
+// server.configure('development', function() {
+//   server
+//     .use(build)
+//     .use(express.errorHandler({
+//       dumpExceptions: true,
+//       showStack: true
+//     }));
+// });
+
+// server.configure('production', function() {
+//   server.use(express.errorHandler());
+// });
+
+server.listen(8002, function() {
+  console.log('Server listening on port: ' + 8002);
 });
 
-server.get('/api/0/docs/:user/:project', function(req, res) {
-  var params = req.params;
-  var docs = [];
-  var parsed;
 
-  finder.open({user: params.user, project: params.project})
-    .on('file', function (file) {
-      if (file.name.match(/.js$/)) {
-        parsed = redox.parse((new Buffer(file.content, 'base64')).toString());
-        docs.push({
-          name: file.name,
-          path: file.path,
-          docs: parsed.docs,
-          codes: parsed.codes
-        });
-      }
-    })
-    .on('end', function() {
-        res.send(docs);
-    });
-});
+// server.get('/:user/:project', function(req, res) {
+//   var params = req.params;
+//   res.redirect('/#/docs/' + params.user + '/' + params.project);
+// });
+
+// server.get('/api/0/docs/:user/:project', function(req, res) {
+//   var params = req.params;
+//   var docs = [];
+//   var parsed;
+
+//   finder.open({user: params.user, project: params.project})
+//     .on('file', function (file) {
+//       if (file.name.match(/.js$/)) {
+//         parsed = redox.parse((new Buffer(file.content, 'base64')).toString());
+//         docs.push({
+//           name: file.name,
+//           path: file.path,
+//           docs: parsed.docs,
+//           codes: parsed.codes
+//         });
+//       }
+//     })
+//     .on('end', function() {
+//         res.send(docs);
+//     });
+// });
